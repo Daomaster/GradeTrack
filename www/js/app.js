450e37eb976d8378e1af7cc8946b23c678b4a79d
Daoyun Zeng <daoyun.zeng@gmail.com>
2016-02-08 15:58:00 -0800

    Added the gitignore and added the graph module to the grading tab

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'controllers', 'services'])
+angular.module('starter', ['ionic', 'controllers', 'services', 'chart.js'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -56,15 +56,6 @@ angular.module('starter', ['ionic', 'controllers', 'services'])
         'tab-grade': {
           templateUrl: 'templates/tab-grade.html',
           controller: 'GradeCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
         }
       }
     })
