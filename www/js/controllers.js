113d49cab2abe962538c81d991bf61be9696b6ef
Daoyun Zeng <daoyun.zeng@gmail.com>
2016-02-08 16:04:09 -0800

    Change the bar-style graph to line style graph

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,9 +17,8 @@ angular.module('controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
-
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
