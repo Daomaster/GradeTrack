77acf09237715f3b517e00379db80450380a1d6e
Daoyun Zeng <daoyun.zeng@gmail.com>
2016-02-08 16:10:10 -0800

    Change to the right time

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,13 +17,13 @@ angular.module('controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.labels = ["Quiz 1", "Quiz 2", "Test 1", "Quiz 3", "Group Project", "Quiz 4", "Final"];
+  $scope.series = ['Average', 'Yours'];
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
     [28, 48, 40, 19, 86, 27, 90]
   ];
-  
+
 })
 
 .controller('AccountCtrl', function($scope) {
