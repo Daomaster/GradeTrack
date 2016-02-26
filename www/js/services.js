741bd51f78b7dabbbe8f7d837c42430a1a088e59
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-02-26 11:37:31 -0800

    Removed difficulty and increase/decrease buttons

--- www/js/services.js
+++ www/js/services.js
@@ -8,7 +8,6 @@ angular.module('services', [])
     {
       label: "HW 1",
       time : new Date(2016, 1, 10),//"Jan 12th 10:40 am",
-	  difficulty: 5,
 	  classId: "#bdf8e6", // will prolly need better way to identify/translate to this
       id : 1,
 	  note: "look at ch2 pg59-62"
@@ -16,7 +15,6 @@ angular.module('services', [])
     {
       label: "HW 1",
       time : new Date(2016, 1, 10),//"Jan 1st 11:40 am",
-	  difficulty: 2,
 	  classId: "#add8e6",
       id : 2,
 	  note: "review big-o"
@@ -24,14 +22,12 @@ angular.module('services', [])
     {
       label: "HW 1",
       time : new Date(2016, 1, 11),//"Jan 1st 11:40 am",
-	  difficulty: 3,
 	  classId: "#adb8e6",
 	  note: "ask that one guy for help"
     },
     {
       label: "HW 1",
 		time : new Date(2016, 1, 12),//"Jan 20th 1:40 pm",
-		difficulty: 5,
 	  classId: "#7cc8e6",
 	  note: "i dont even go here"
     }
@@ -41,17 +37,6 @@ angular.module('services', [])
     all: function() {
 		var x = 0;
 		var j = 0;
-		
-	   // insertion sort by difficulty
-	   for (var i = 1; i < schedules.length; i++) {
-		  x = schedules[i];
-		  j = i - 1;
-		  while (j >= 0 && schedules[j].difficulty < x.difficulty) {
-			  schedules[j + 1] = schedules[j];
-			  j = j - 1;
-		  }
-		  schedules[j + 1] = x;
-	  }
 		
 	  // insertion sort by date object
 	  for (var i = 1; i < schedules.length; i++) {
@@ -76,13 +61,7 @@ angular.module('services', [])
         }
       }
       return null;
-    },
-	plusOne: function(index) {
-		schedules[index].difficulty++;
-	},
-	minusOne: function(index) {
-		schedules[index].difficulty--;
-	}
+    }
   };
 })
 
